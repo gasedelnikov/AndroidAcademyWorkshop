79cecbca9bd84bf8184a9a02322bab646e54eb9b
grse1118 <grigorii.sedelnikov@netcracker.com>
2020-11-21 13:34:00 +0300

    add start and End Guidelines

--- app/src/main/java/com/gri/androidacademyworkshop/MovieDetailsActivity.kt
+++ app/src/main/java/com/gri/androidacademyworkshop/MovieDetailsActivity.kt
@@ -8,8 +8,6 @@ class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
-//        setSupportActionBar(findViewById(R.id.toolbar))
-
     }
 
 
